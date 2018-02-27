740a2d66877091d981a68dfdf9cf4af8eb2b5946
Breno Batista Machado <breno.machado@montreal.com.br>
2018-02-27 18:45:14 -0300

    Creating PoC using SQLite

---
+++ CustomerSuccessWeb/DataLayer/CustomerSuccessContext.cs
@@ -0,0 +1,20 @@
+﻿using CustomerSuccessWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerSuccessWeb.DataLayer
+{
+    public class CustomerSuccessContext: DbContext
+    {
+        public CustomerSuccessContext(DbContextOptions<CustomerSuccessContext> options)
+            : base (options)
+        {
+            this.Database.Migrate();
+        }
+
+        public DbSet<Client> Clients { get; set; }
+    }
+}
--- CustomerSuccessWeb/Startup.cs
+++ CustomerSuccessWeb/Startup.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CustomerSuccessWeb.DataLayer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -22,6 +24,8 @@ namespace CustomerSuccessWeb
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<CustomerSuccessContext>(options => options.UseSqlite("Data Source=CustomerSuccessDB.db"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
